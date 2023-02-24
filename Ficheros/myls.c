9fbcf3ce1736e86b388b7d18e13d74bbac0b5581
DVSS <99610648+Lizzen@users.noreply.github.com>
2023-02-24 12:50:00 +0100

    Update myls.c

--- Ficheros/myls.c
+++ Ficheros/myls.c
@@ -10,37 +10,47 @@ int main(int argc, char *argv[]) {
     struct stat sb;
     struct dirent *entrada;
 
-    // Manejo de opciones de línea de comando
     while ((opt = getopt(argc, argv, "d:f:")) != -1) {
         switch (opt) {
             case 'd':
                 path = optarg;
-                if (lstat(path, &sb) == 0 && S_ISDIR(sb.st_mode) && (sb.st_mode & S_IRWXU) ) {
-                    DIR *dir = opendir(path);
+
+                // Abre el fichero si cumle las condiciones
+                if (lstat(path, &sb) == 0 && S_ISDIR(sb.st_mode) && (sb.st_mode & S_IRWXU) ) { 
+                    DIR *dir = opendir(path);    
 
                     if (dir == NULL) {
                         perror("opendir");
                         exit(EXIT_FAILURE);
                     }
 
-                    while ((entrada = readdir(dir)) != NULL) {
-                        if (lstat(entrada->d_name, &sb) == 0) {
-                            if (S_ISREG(sb.st_mode)) { // Fichero Regular
-                                printf("%s (%.1f kb, %ld link%s)", entrada->d_name, (double) (sb.st_size / 1024), sb.st_nlink, sb.st_nlink == 1 ? "" : "s");
-                                if (sb.st_mode & S_IXUSR || sb.st_mode & S_IXGRP || sb.st_mode & S_IXOTH) {
-                                    printf("*");
-                                }
-                                printf("\n");
-                            } else if (S_ISDIR(sb.st_mode)) { // Directorio
-                                printf("[%s] (%ld link%s)\n", entrada->d_name, sb.st_nlink, sb.st_nlink == 1 ? "" : "s");
-                            } else if (S_ISLNK(sb.st_mode)) { // Enlace Simbólico
-                                char link_target[PATH_MAX];
-                                ssize_t link_size = readlink(entrada->d_name, link_target, (double) (sb.st_size / 1024));
-                                link_target[link_size] = '\0';
-                                printf("%s (%s)\n", entrada->d_name, link_target);
+                     // Lee cada entrada del directorio
+                    while ((entrada = readdir(dir)) != NULL) {       
+                            // Abre cada entrada
+                            if (lstat(entrada->d_name, &sb) == 0) {
+                                   if (S_ISREG(sb.st_mode)) { // Fichero Regular
+                                        printf("%s (%.1f kb, %ld link)", entrada->d_name, (double) (sb.st_size / 1024), sb.st_nlink);
+                                        if (sb.st_mode & S_IXUSR || sb.st_mode & S_IXGRP || sb.st_mode & S_IXOTH) {
+                                            printf("*");
+                                        }
+                                        printf("\n");
+                                   } else if (S_ISDIR(sb.st_mode)) { // Directorio
+                                        printf("[%s] (%ld link)\n", entrada->d_name, sb.st_nlink);
+                                   } else if (S_ISLNK(sb.st_mode)) { // Enlace Simbólico
+                                        char *buffer = malloc(sb.st_size + 1);
+                                        if (buffer == NULL){
+                                            perror("malloc");
+                                            exit(EXIT_FAILURE);
+                                        }
+                                        ssize_t linkdat = readlink(entrada->d_name, buffer, sb.st_size);
+                                        if (linkdat == -1){
+                                            perror("readlink");
+                                            exit(EXIT_FAILURE);
+                                        }
+                                        printf("%s (%ld)\n", entrada->d_name, linkdat);
+                                   }
                             }
-                        }
-                    }
+                     }
                     closedir(dir);
                 } else {
                     printf("El archivo no es ejecutable\n");
@@ -49,10 +59,11 @@ int main(int argc, char *argv[]) {
                 break;
             case 'f':
                 path = optarg;
-                if (lstat(path, &sb) == 0 && S_ISREG(sb.st_mode)) {
+                // Abre el fichero regular
+                if (lstat(path, &sb) == 0 && S_ISREG(sb.st_mode)) { 
                     printf("%s (inodo %ld, %.1f kb)\n", path, sb.st_ino, (double) (sb.st_size / 1024));
                 } else {
-                    printf("%s no es un fichero regular\n", path);
+                    printf("No es un fichero regular\n");
                     exit(EXIT_FAILURE);
                 }
                 break;
