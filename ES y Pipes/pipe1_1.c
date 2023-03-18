1899a24255d174c7492229ee62fda70144e54151
DVSS <99610648+Lizzen@users.noreply.github.com>
2023-03-18 19:08:11 +0100

    Finished Pipe1_1

--- ES y Pipes/pipe1_1.c
+++ ES y Pipes/pipe1_1.c
@@ -3,27 +3,24 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <time.h>
+
+#define PIPE_NAME "~/mituberia"
 
 int main() {
     struct stat st;
-    char *fifo_path = "~/mituberia";
-    int res = mkfifo(fifo_path, 0666);
 
-    if (res == -1) {
+    if (mkfifo(PIPE_NAME, 0666) == -1) {
         perror("Error al crear la tuberia");
         exit(EXIT_FAILURE);
     }
 
-    stat(fifo_path, &st);
-
-    printf("Tipo de fichero: %d\n", st.st_mode & S_IFMT);
-    printf("Permisos: %o\n", st.st_mode & 0777);
-    printf("Propietario: %d\n", st.st_uid);
-    printf("Grupo: %d\n", st.st_gid);
-    printf("Tamaño: %ld\n", st.st_size);
-    printf("Último acceso: %ld\n", st.st_atime);
-    printf("Última modificación: %ld\n", st.st_mtime);
-    printf("Último cambio de estado: %ld\n", st.st_ctime);
+    if (stat(PIPE_NAME, &st) != -1){
+        printf("Tamaño de la tubería: %lld bytes\n", (long long) st.st_size);
+        printf("Permisos de la tubería: %o\n", st.st_mode & 0777);
+        printf("Último acceso a la tubería: %s", ctime(&st.st_atime));
+        printf("Última modificación de la tubería: %s", ctime(&st.st_mtime));
+    }
     
     return 0;
 }
