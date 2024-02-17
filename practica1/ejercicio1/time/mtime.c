31b28b124a1a8955b70b8fb9baa1f62f89ff9774
Lizzen <dferre07@ucm.es>
2024-02-17 12:57:32 +0100

    ejercicio 5 completado

--- practica1/ejercicio1/time/mtime.c
+++ practica1/ejercicio1/time/mtime.c
@@ -1,17 +1,21 @@
 // includes
+#include <stdio.h>
+#include <time.h>
 int main(){
-  
+    time_t now = time(NULL);
 
     char *shoy, *s;
+    shoy = ctime(&now);
 
     // consular la fecha actual
 
-  
     printf("Hoy es: %s",shoy);
  
 
     // calcular que fecha fue hace 10 dias
-    
+
+    time_t diez_dias = now - (10*24*60*60);
+    s = ctime(&diez_dias);
     printf("Hace 10 dias fue: %s",s);    
     
     return 0;
