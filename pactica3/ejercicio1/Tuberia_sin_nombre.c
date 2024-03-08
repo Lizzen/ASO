148fe65f356f8a993f13d5890df74593af63ef67
Lizzen <dferre07@ucm.es>
2024-03-08 20:25:27 +0100

    update

---
+++ pactica3/ejercicio1/Tuberia_sin_nombre.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    if (argc < 5){
+        printf("Faltan argumentos: ./programa comando1 argumento 1 | comando2 argumento2\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int pipe_fd[2];
+
+    //Crea tubería sin nombre
+    if (pipe(pipe_fd) == -1){
+        perror("pipe: Error al crear la tubería sin nombre.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Crea proceso primer hijo
+    pid_t pid = fork();
+    
+    if (pid == -1){
+        perror("fork: Error al crear hijo1.\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid == 0){
+
+        //Cierra salida estandar de escritura
+        if (close(STDOUT_FILENO) == -1){
+            perror("close: Error al cerrar STDOUT_FILENO.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Duplica la salida estandar con el extremo de escritura de la tubería
+        if (dup2(pipe_fd[0], STDOUT_FILENO) == -1){
+            perror("dup2: Error al duplicar la salida estandar con el extremo de escritura.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Cierra extremo de lectura y escritura que no vamos a usar 
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+
+        //Ejecuta comando1 argumento1
+        if (execlp(argv[1], argv[2], NULL) == -1) {
+            perror("execlp: Error al ejecutar ls -l.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pid_t pid2 = fork();
+    
+    if (pid2 == -1){
+        perror("fork: Error al crear hijo2.\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid2 == 0){
+
+        //Cierra salida estandar de escritura
+        if (close(STDIN_FILENO) == -1){
+            perror("close: Error al cerrar STDIN_FILENO.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Duplica la salida estandar con el extremo de escritura de la tubería
+        if (dup2(pipe_fd[1], STDOUT_FILENO) == -1){
+            perror("dup2: Error al duplicar la salida estandar con el extremo de escritura.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //Cierra extremo de lectura y escritura que no vamos a usar 
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+
+        //Ejecuta comando2 argumento2
+        if (execlp(argv[3], argv[4], NULL) == -1) {
+            perror("execlp: Error al ejecutar wc -l.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //Cierra extremo de lectura y escritura que no vamos a usar en el proceso padre
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+
+    waitpid(pid, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    return 0;
+}
