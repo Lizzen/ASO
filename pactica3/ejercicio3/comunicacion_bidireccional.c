b8d762f10bc8f77055380a2c88cc654ba7205dcb
Lizzen <dferre07@ucm.es>
2024-03-18 21:23:26 +0100

    da error por la cara

--- pactica3/ejercicio3/comunicacion_bidireccional.c
+++ pactica3/ejercicio3/comunicacion_bidireccional.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+#define READ_PIPE 0
+#define WRITE_PIPE 1
+
 int main(){
     int pipe_padre[2], pipe_hijo[2];
 
@@ -23,10 +26,42 @@ int main(){
     }
     else if (pid == 0){
         // Proceso hijo
-        
+        close(pipe_hijo[READ_PIPE]);
+        close(pipe_padre[WRITE_PIPE]);
+
+        char buffer[256];
+
+        if (read(pipe_padre[READ_PIPE], buffer, sizeof(buffer) == -1){
+            perror("read: Error al leer del pipe.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; buffer[i]; ++i){
+            buffer[i] = toupper(buffer[i]);
+        }
+
+        printf("Mensaje recibido del padre:\n %s\n", buffer);
+        sleep(1); 
+        if (write(pipe_hijo[WRITE_PIPE], 'n', 1) == -1){
+            perror("write: Error al escribir en el pipe del hijo_padre.\n");
+            exit(EXIT_FAILURE);
+        }
     }
     else{
         // Proceso padre
+        close(pipe_padre[READ_PIPE]);
+        close(pipe_hijo[WRITE_PIPE]);
         
+        char buffer[256];
+        char hijo;
+        while (hijo != 'q'){
+            printf("Inserta un mensaje: ");
+            fgets(buffer, sizeof(buffer), stdin);
+            write(pipe_padre[WRITE_PIPE]);
+            read(pipe_hijo[READ_PIPE], &hijo, sizeof(hijo));
+            wait(NULL); // Wait for child to finish
+        }
+        close(pipe_padre[WRITE_PIPE]);
+        close(pipe_hijo[READ_PIPE]);      
     }
- }
+}
